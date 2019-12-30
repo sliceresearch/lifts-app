32f3092a26b09448f100823fce57cada1db9b39a
Damian Hills <damski@gmail.com>
2019-12-30 11:28:37 +1000

    conf

--- conf/lifts-build-image-anet-pull.sh
+++ conf/lifts-build-image-anet-pull.sh
@@ -1,12 +1,5 @@
 
 
-########################################### gitlab build
-
-sudo docker login --username=damski@gmail.com registry.gitlab.com --password-stdin < ~/.ssh/dock_pw_gitlab
-
-sudo docker build --build-arg host_id=lifts --build-arg host_name=lifts -t registry.gitlab.com/damski/lifts -f ./conf/docker-build-file .
-sudo docker push registry.gitlab.com/damski/lifts
-
 #### remote site: damski@www.assimilate.net
 ssh -t damski@www.assimilate.net 'sudo docker system prune -af'
 ssh -t damski@www.assimilate.net 'sudo docker login --username=damski@gmail.com registry.gitlab.com --password-stdin < ~/.ssh/dock_pw_gitlab'
