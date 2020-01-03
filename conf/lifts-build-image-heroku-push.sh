335b4257787eef1646560820f7138cabb1297072
Damian Hills <damski@gmail.com>
2020-01-03 15:13:33 +1000

    conf

--- conf/lifts-build-image-heroku-push.sh
+++ conf/lifts-build-image-heroku-push.sh
@@ -6,9 +6,9 @@ cd /lab/lifts
 
 sudo docker system prune -af
 
-cat  ~/.ssh/herokupw
+#cat  ~/.ssh/herokupw
 
-sudo heroku login -i #damski@gmail.com  < ~/.ssh/herokupw
+#sudo heroku login -i #damski@gmail.com  < ~/.ssh/herokupw
 sudo heroku container:login
 
 sudo docker build --build-arg host_id=lifts --build-arg host_name=lifts -t liftsappheroku -f ./conf/lifts-build-dockerfile .
