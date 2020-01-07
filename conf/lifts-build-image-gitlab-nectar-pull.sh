03f7067accbb241ce57b5b28bed90b8764285dd6
Damian Hills <damski@gmail.com>
2020-01-07 16:04:02 +1000

    conf

--- conf/lifts-build-image-gitlab-nectar-pull.sh
+++ conf/lifts-build-image-gitlab-nectar-pull.sh
@@ -4,7 +4,7 @@
 ssh -t ubuntu@115.146.85.9 'sudo docker system prune -af'
 ssh -t ubuntu@115.146.85.9 'sudo docker login --username=damski@gmail.com registry.gitlab.com --password-stdin < ~/.ssh/dock_pw_gitlab'
 ssh -t ubuntu@115.146.85.9 'sudo docker pull registry.gitlab.com/damski/lifts-app'
-ssh -t ubuntu@115.146.85.9 '/lab/lifts/conf/lifts-run.sh'
+ssh -t ubuntu@115.146.85.9 '/home/ubuntu/lifts-run.sh'
 
 exit
 
