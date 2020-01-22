ce872b486b0de7d2d4449444033fabce6daf5a7a
Damian Hills <damski@gmail.com>
2020-01-22 16:32:22 +1000

    uploadcode

--- src/app/appx/appx.module.ts
+++ src/app/appx/appx.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppXComponent } from './appx.component';
 
+
+import { FileSelectDirective } from 'ng2-file-upload';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  imports: [CommonModule],
-  declarations: [AppXComponent],
+  imports: [CommonModule,FormsModule],
+  declarations: [AppXComponent,FileSelectDirective],
   exports: [AppXComponent]
 })
 export class AppXModule {}
+
